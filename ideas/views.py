5e993747bf703589185dc1dd60636c1631916de4
dhorsten101 <dhorsten101@gmail.com>
2025-03-30 02:03:26 +0200

    ideas

--- ideas/views.py
+++ ideas/views.py
@@ -51,37 +51,3 @@ def idea_delete(request, id):
 		idea.delete()
 		return redirect("idea_list")
 	return render(request, "idea_confirm_delete.html", {"idea": idea})
-
-# @login_required
-# def delete_image(request, image_id):
-# 	image = get_object_or_404(SellableImage, id=image_id)
-#
-# 	# Keep track of the associated sellable
-# 	sellable = image.sellable
-#
-# 	# Delete the image
-# 	image.delete()
-#
-# 	# Redirect back to the sellable detail page
-# 	return redirect('sellable_detail', pk=sellable.pk)
-
-#
-
-# @login_required
-# def sellable_detail(request, pk):
-# 	sellable = get_object_or_404(Idea, pk=pk)
-#
-# 	# Handle image upload
-# 	if request.method == "POST" and 'image' in request.FILES:
-# 		image_form = SellableImageForm(request.POST, request.FILES)
-# 		if image_form.is_valid():
-# 			image = image_form.save(commit=False)
-# 			image.sellable = sellable  # Associate with the current sellable
-# 			image.save()
-# 			return redirect("sellable_detail", pk=sellable.pk)
-#
-# 	else:
-# 		image_form = SellableImageForm()
-#
-# 	return render(request, "sellable_detail.html", {"sellable": sellable, "image_form": image_form}, )
-#

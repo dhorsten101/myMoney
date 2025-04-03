eddf236d675c51a83bbc4db7b9cacca47d2f2726
dhorsten101 <dhorsten101@gmail.com>
2025-04-03 23:07:47 +0200

    central history

--- expenses/models.py
+++ expenses/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class Expense(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    balance = models.DecimalField(max_digits=10, decimal_places=6)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f"{self.name} - {self.balance}"
+	name = models.CharField(max_length=100)
+	description = models.TextField()
+	balance = models.DecimalField(max_digits=20, decimal_places=6)
+	
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	
+	def __str__(self):
+		return f"{self.name} - {self.balance}"

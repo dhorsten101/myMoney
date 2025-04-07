8da7009c725c3f521107f30da3a5f682486bdb03
dhorsten101 <dhorsten101@gmail.com>
2025-04-07 14:40:59 +0200

    crypto stats

--- crypto/models.py
+++ crypto/models.py
@@ -17,3 +17,20 @@ class Crypto(models.Model):
 	
 	def __str__(self):
 		return f"{self.exchange} - {self.name} - {self.balance}"
+
+
+from django.db import models
+
+
+class CryptoRecord(models.Model):
+	EXCHANGE_CHOICES = [
+		("binance", "Binance"),
+		("luno", "Luno"),
+	]
+	
+	total_value = models.DecimalField(max_digits=20, decimal_places=2)
+	exchange = models.CharField(max_length=50, choices=EXCHANGE_CHOICES)
+	timestamp = models.DateTimeField(auto_now_add=True)
+	
+	def __str__(self):
+		return f"{self.exchange} - {self.total_value}"

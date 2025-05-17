47dc97a7794a7b1df69faef5089a17171ff3e18e
dhorsten101 <dhorsten101@gmail.com>
2025-05-17 19:10:41 +0200

    added worth model

--- worth/models.py
+++ worth/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Worth(models.Model):
+	CATEGORY_CHOICES = [
+		("property", "Property"),
+		("houses", "Houses"),
+		("cash", "Cash"),
+		("investments", "Investments"),
+		("shares", "Shares"),
+		("valuables", "Valuables"),
+		("crypto", "Crypto"),
+	]
+	
+	name = models.CharField(max_length=100)
+	quick_value = models.DecimalField(max_digits=20, decimal_places=2)
+	real_value = models.DecimalField(max_digits=20, decimal_places=2)
+	notes = models.TextField()
+	category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	
+	def __str__(self):
+		return f"{self.name}"

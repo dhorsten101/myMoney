f9662364708c2937e729d52aac6e786030a3a13e
dhorsten101 <dhorsten101@gmail.com>
2025-07-23 13:41:50 +0200

    slow queries

--- logs/views.py
+++ logs/views.py
@@ -1,10 +1,9 @@
-from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
 
 from .models import SlowQueryLog
 
 
-@staff_member_required
+# @staff_member_required
 def slow_queries_view(request):
 	page_obj = SlowQueryLog.objects.all()[:50]
 	return render(request, 'slow_queries.html', {'page_obj': page_obj})
